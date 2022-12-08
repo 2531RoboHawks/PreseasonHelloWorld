787207e2900daf0fe9834c57712a5a176397edc8
FluffyTailK <111025342+FluffyTailK@users.noreply.github.com>
2022-12-08 17:35:25 -0600

    firstCommit

--- baseHelloWorld.java
+++ baseHelloWorld.java
@@ -1,6 +1,7 @@
 // Create hello world program
 public class baseHelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Kaitlyn");
+        System.out.println("Yay it works ");
     }
 }
