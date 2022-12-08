a770ff6777d0ffa86230766249e70e1f71009b1b
LemonHead2114 <119543677+LemonHead2114@users.noreply.github.com>
2022-12-08 17:39:05 -0600

    Revert "hi"

--- baseHelloWorld.java
+++ baseHelloWorld.java
@@ -1,6 +1,6 @@
 // Create hello world program
 public class baseHelloWorld {
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("Hello World!");
     }
 }
