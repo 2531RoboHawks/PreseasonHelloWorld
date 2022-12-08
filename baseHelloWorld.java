c8238bd85cd71d638e6cf41a836be5b46e224fcf
A. Hanson <65613848+WaterGame2023@users.noreply.github.com>
2022-12-08 17:29:22 -0600

    Tada

--- baseHelloWorld.java
+++ baseHelloWorld.java
@@ -1,6 +1,6 @@
 // Create hello world program
 public class baseHelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Ooogly boogly y'all ive come to steal your kneecaps.  Enjoy the suffering under my domain. >:)");
     }
 }
