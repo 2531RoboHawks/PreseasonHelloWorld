6aa2dba0423f0663ad94ede433a657e9232876af
crazyirishmaniac <Crazyirishmaniac@outlook.com>
2022-12-08 17:43:18 -0600

    Update baseHelloWorld.java

--- baseHelloWorld.java
+++ baseHelloWorld.java
@@ -1,6 +1,6 @@
 // Create hello world program
 public class baseHelloWorld {
     public static void main(String[] args) {
-        System.out.println("HELLO GROOLS!!!!!!!");
+        System.out.println("HELLO Kit Kats!!!!!!! those toe beans are lookin might tasty");
     }
 } 
