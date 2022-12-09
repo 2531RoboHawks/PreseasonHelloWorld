17c80671fe4532c7401fb965865963b6cecc4b4c
25kingk <95448028+25kingk@users.noreply.github.com>
2022-12-08 18:06:49 -0600

    commit

--- baseHelloWorld.java
+++ baseHelloWorld.java
@@ -1,6 +1,15 @@
 // Create hello world program
 public class baseHelloWorld {
     public static void main(String[] args) {
-        System.out.println("Kyle King");
+        System.out.println("Heavy: Ya da, ya da da da IT IS GOOD DAY TO BE NOT DED!");
+        System.out.println("Engineer: Pow! you are ded!");
+        System.out.println("Heavy: I am Ded!");
+        System.out.println("Engineer: *chuckles*");
+        System.out.println("Psy: *approaching noises*");
+        System.out.println("Engineer: Aw shucks");
+        System.out.println("Spy: *le gasp*");
+        System.out.println("Spy: The Heavy is ded!");
+        System.out.println("Heavy: Yes, I am DED!");
+        System.out.println("Spy: Why is the heavy ded?");
     }
 }
